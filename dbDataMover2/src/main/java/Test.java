908fa4a2afe85adcc148b709835953977235a705
Dmitriy <dyav2017@gmail.com>
2021-09-07 20:21:33 +0300

    new

---
+++ dbDataMover2/src/main/java/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args) {
+
+        for (int n = 0; n < 999; n = n + 10) {
+            for (int j = n; j < n+10; j++) {
+                System.out.print(j+" ");
+
+            }
+            System.out.println();
+
+//
+        }
+    }
+}
